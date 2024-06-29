200d9e13ae2ab1b96c4ddd13e130cc4e29e35aaa
Olakunle Boye <kunlexzy@gmail.com>
2024-06-29 20:50:20 +0100

    added caddy config file

---
+++ .docker/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "🎬 entrypoint.sh"
+
+composer dump-autoload --no-interaction --no-dev --optimize
+
+echo "🎬 artisan commands"
+
+# php artisan cache:clear
+# php artisan migrate --no-interaction --force
+# php artisan db:seed --no-interaction --force
+
+echo "🎬 start supervisord"
+
+supervisord -c $APP_PATH/.docker/php/supervisor.conf
